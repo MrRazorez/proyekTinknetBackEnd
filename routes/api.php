98fbaf1bb8cfbef904cd2b1bf690f5e9dbdacaa0
MrRazorez <viantosa1999@gmail.com>
2022-06-28 12:24:28 +0700

    Perubahan

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userTinknetController;
+use App\Http\Controllers\officeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\userTinknetController;
 */
 
 Route::get("usertinknet", [userTinknetController::class, "validAccount"]);
+Route::get("dataoffice", [officeController::class, "index"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
